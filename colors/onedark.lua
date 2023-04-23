e895bb4164c7f0dc9c13b4e18c6cb44b7da5f1c2
Ghasak <mdotgh@gmail.com>
2023-04-24 00:56:14 +0900

    [colorls theme] Imporved the colorls theme for our worflow 2.0

--- colors/onedark.lua
+++ colors/onedark.lua
@@ -2,5 +2,11 @@ for k in pairs(package.loaded) do
     if k:match(".*onedark.*") then package.loaded[k] = nil end
 end
 
-require('onedark').setup()
-require('onedark').colorscheme()
+-- require('onedark').setup()
+-- require('onedark').colorscheme()
+
+local onedark_status, onedark = pcall(require, "onedark")
+if onedark_status then
+	onedark.setup({})
+	onedark.colorscheme()
+end
