ad11cc4555de8f37b2e3d554d38a6aa8ea067cd4
Ghasak <mdotgh@gmail.com>
2023-04-23 22:34:34 +0900

    [colorls theme] Imporved the colorls theme for our worflow 2.0

--- lua/lualine/themes/onedark.lua
+++ lua/lualine/themes/onedark.lua
@@ -15,20 +15,22 @@ local colors = {
 local one_dark = {
     inactive = {
         a = {fg = colors.gray, bg = colors.bg, gui = 'bold'},
-        --b = {fg = colors.gray, bg = colors.bg},
-        b = {fg = colors.gray, bg = c.purple},
-        c = {fg = colors.gray, bg = cfg.lualine.transparent and c.none or c.bg1},
+        b = {fg = colors.gray, bg = c.grey},
+        c = {
+            fg = colors.gray,
+            bg = cfg.lualine.transparent and c.none or c.black
+        }
     },
     normal = {
         a = {fg = colors.bg, bg = colors.green, gui = 'bold'},
         b = {fg = colors.fg, bg = c.grey},
-        --c = {fg = colors.fg, bg = cfg.lualine.transparent and c.none or c.bg1},
-        c = {fg = colors.fg, bg = cfg.lualine.transparent and c.none or c.red},
+        c = {fg = colors.fg, bg = cfg.lualine.transparent and c.none or c.black}
+        -- c = {fg = colors.fg, bg = cfg.lualine.transparent and c.none or c.red},
     },
     visual = {a = {fg = colors.bg, bg = colors.purple, gui = 'bold'}},
     replace = {a = {fg = colors.bg, bg = colors.red, gui = 'bold'}},
     insert = {a = {fg = colors.bg, bg = colors.blue, gui = 'bold'}},
     command = {a = {fg = colors.bg, bg = colors.yellow, gui = 'bold'}},
-    terminal = {a = {fg = colors.bg, bg = colors.cyan, gui = 'bold'}},
+    terminal = {a = {fg = colors.bg, bg = colors.cyan, gui = 'bold'}}
 }
 return one_dark;
