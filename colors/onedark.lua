4c971a0f960de5c668df6c8d3c12b67bd8fbe843
Ghasak <mdotgh@gmail.com>
2023-04-24 00:59:14 +0900

    [colorls theme] Imporved the colorls theme for our worflow 2.0

--- colors/onedark.lua
+++ colors/onedark.lua
@@ -1,5 +1,6 @@
 for k in pairs(package.loaded) do
-    if k:match(".*onedark.*") then package.loaded[k] = nil end
+    --if k:match(".*onedark.*") then package.loaded[k] = nil end
+    if k:match(".*githubG.*") then package.loaded[k] = nil end
 end
 
 -- require('onedark').setup()
