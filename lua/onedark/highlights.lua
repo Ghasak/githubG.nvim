a8686aa13840a6c16ef86ed1b83b3570267041eb
Ghasak <mdotgh@gmail.com>
2023-10-26 23:02:14 +0900

    ...

--- lua/onedark/highlights.lua
+++ lua/onedark/highlights.lua
@@ -660,7 +660,7 @@ hl.langs.markdown = {
 	markdownBlockquote = colors.Grey,
 	markdownBold = { fg = c.none, fmt = "bold" },
 	markdownBoldDelimiter = colors.Grey,
-	markdownCode = { fg = c.white, bg = c.github_symbol, fmt = "bold" },
+	markdownCode = { fg =c.pale_dogwood , bg = c.github_symbol, fmt = "bold" },
 	markdownCodeBlock = colors.Green,
 	markdownCodeDelimiter = colors.Yellow,
 	markdownH1 = { fg = c.red, fmt = "bold" },
